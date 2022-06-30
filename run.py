1499c308f6d41d783c1b0f9eda3779cd313eec2f
admin <admin>
2022-06-30 16:32:26 +0800

    修改报告路径

--- run.py
+++ run.py
@@ -4,11 +4,6 @@ import time
 import pytest
 
 if __name__ == '__main__':
-
-    # pytest.main(['-vs','./testcase/visca_test.py::Test_visca::test_Color_Temperature'])
     pytest.main(['-vs','./testcase/visca_test.py'])
-
-    # pytest.main(['-vs','./testcase/1.py'])
-    # pytest.main(['-vs','./testcase/test_1.py'])
     time.sleep(3)#生成临时报告的时间
     os.system('allure generate ./allure-results -o ./allure-report --clean')#生成正式报告
