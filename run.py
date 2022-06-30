6b1ccdb6b6ac8b818e6b912923bdda71ba979e15
admin <admin>
2022-06-30 13:47:31 +0800

    修改报告路径

--- run.py
+++ run.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     # pytest.main(['-vs','./testcase/1.py'])
     # pytest.main(['-vs','./testcase/test_1.py'])
     time.sleep(3)#生成临时报告的时间
-    os.system('allure generate ./temp -o ./report --clean')#生成正式报告
+    os.system('allure generate ./allure-results -o ./allure-report --clean')#生成正式报告
