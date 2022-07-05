6de237293c56129e3add5c77b78e4e91b1ac8467
baoyl <1506362472@qq.com>
2022-07-05 15:59:59 +0800

    运行所有Visca命令测试用例

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ import pytest
 if __name__ == '__main__':
 
 
-    pytest.main(['-vs','--alluredir', './temp','./testcase/visca_test.py::Test_visca::test_power'])
+    pytest.main(['-vs','--alluredir', './temp','./testcase/visca_test.py'])
     time.sleep(3)#生成临时报告的时间
     os.system('allure generate ./temp -o ./allure-report --clean')#生成正式报告
