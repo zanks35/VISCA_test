a8a5b8a006402b7933142af1ade7d6a7b0863fa8
baoyl <1506362472@qq.com>
2022-07-05 15:49:21 +0800

    pytest.main(['-vs','--alluredir', './temp','./testcase/visca_test.py::Test_visca::test_power'])

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ import pytest
 if __name__ == '__main__':
 
 
-    pytest.main(['-vs','./testcase/visca_test.py'])
+    pytest.main(['-vs','--alluredir', './temp','./testcase/visca_test.py::Test_visca::test_power'])
     time.sleep(3)#生成临时报告的时间
     os.system('allure generate ./temp -o ./report --clean')#生成正式报告
