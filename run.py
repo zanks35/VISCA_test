9abdcace232d0863dc87e14c64c51277a36ffb92
baoyl <1506362472@qq.com>
2022-07-05 15:02:47 +0800

    os.system('allure generate ./temp -o ./report --clean')#生成正式报告

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
 
     pytest.main(['-vs','./testcase/visca_test.py'])
     time.sleep(3)#生成临时报告的时间
-    os.system('allure generate ./temp -o ./allure-report --clean')#生成正式报告
+    os.system('allure generate ./temp -o ./report --clean')#生成正式报告
